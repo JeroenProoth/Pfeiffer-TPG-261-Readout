3429af202e5fdd2deb6523f28a3858044db77003
Jeroen Prooth <33283073+JeroenProoth@users.noreply.github.com>
2020-12-16 14:05:42 +0100

    Code cleanup

--- main.py
+++ main.py
@@ -1,53 +1,54 @@
-import pyvisa
 import time
-
 from datetime import date
+
+import pyvisa
+
+
 resourceManager = pyvisa.ResourceManager()
 resources = resourceManager.list_resources()
-print(resources)
 
 freq_counter = resourceManager.open_resource('GPIB0::21::INSTR')
 pressure_gauge_turbo = resourceManager.open_resource('ASRL4::INSTR')
 pressure_gauge_evap = resourceManager.open_resource('ASRL3::INSTR')
 
 for device in [freq_counter, pressure_gauge_turbo, pressure_gauge_evap]:
-	device.timeout = 2000
-	device.write_termination = '\n'
-	device.read_termination ='\n'
-	device.baud_rate = 9600 
+    device.timeout = 2000
+    device.write_termination = '\n'
+    device.read_termination ='\n'
+    device.baud_rate = 9600
 
-	print('{} : {}.'.format(str(device), device.query('*IDN?')))
+    print('{} : {}.'.format(str(device), device.query('*IDN?')))
 
 
-# material = 'Diamantane-10-3mbar-60C'
-material = 'QuartzAbove-Ambient-None'
-amount = ''
-date = date.today().strftime("%d-%m-%Y")		
-filename = "data\\" + date + '_' + material + '_' + amount + '.txt'
+MATERIAL = ''
+AMOUNT = ''
+date = date.today().strftime("%d-%m-%Y")
+filename = "data\\" + date + '_' + MATERIAL + '_' + AMOUNT + '.txt'
 
 freq_counter.write(':EVENT1:LEVEL -.05')
 time_start = time.time()
 with open(filename, "a") as file:
-		file.write('Time (s)' + '\t' + 'Pressure Turbo (mbar)' + '\t' + 'Pressure Evap (mbar)' + '\t' + 'Frequency (Hz)' '\n')
+    file.write(
+        'Time (s)' + '\t' + 'Pressure Turbo (mbar)' + '\t' + 'Pressure Evap (mbar)'
+        + '\t' + 'Frequency (Hz)' '\n'
+    )
+
 while True:
-	u_turbo = float(pressure_gauge_turbo.query('READ?').split(",")[0])
-	p_turbo = pow(10, 1.667*u_turbo-11.33)
-	if p_turbo > 1000:
-		p_turbo = 1000
+    u_turbo = float(pressure_gauge_turbo.query('READ?').split(",")[0])
+    p_turbo = pow(10, 1.667*u_turbo-11.33)
+    if p_turbo > 1000:
+        p_turbo = 1000
 
-	u_evap = float(pressure_gauge_evap.query('READ?').split(",")[0])
-	p_evap = pow(10, 1.667*u_evap-11.33)
-	if p_evap > 1000:
-		p_evap = 1000
+    u_evap = float(pressure_gauge_evap.query('READ?').split(",")[0])
+    p_evap = pow(10, 1.667*u_evap-11.33)
+    if p_evap > 1000:
+        p_evap = 1000
 
-	f = float(freq_counter.query('READ?'))
+    f = float(freq_counter.query('READ?'))
 
-	time_passed = time.time() - time_start
-	with open(filename, "a") as file:
-		line = str(time_passed) + '\t' + str(p_turbo) + '\t' + str(p_evap) + '\t' +str(f) +  '\n'
-		print(line)
-		file.write(line)
+    time_passed = time.time() - time_start
+    with open(filename, "a") as file:
+        line = str(time_passed) + '\t' + str(p_turbo) + '\t' + str(p_evap) + '\t' +str(f) +  '\n'
+        file.write(line)
 
-	time.sleep(1)
-
- 
+    time.sleep(1)
--- reduce_size.py
+++ reduce_size.py
@@ -1,98 +1,106 @@
-import os, sys, getopt
+import os
+import sys
+import getopt
 import pandas as pd
 
 def adaptive_reduce_size(dataframe, error = 0.0005):
-	data_list = dataframe.values.tolist()
+    '''Reduces dataframe size by excluding any variations
+    below a certain error (%) change.
+    '''
+    data_list = dataframe.values.tolist()
 
-	new_list = [data_list[0]]
+    new_list = [data_list[0]]
 
-	for index in range(1, len(data_list)):
-		if (data_list[index][1] < (new_list[-1][1] * (1 - error))) or (data_list[index][1] >= (new_list[-1][1] * (1 + error))):
-			new_list.append(data_list[index])
+    for index in range(1, len(data_list)):
+        if (data_list[index][1] < (new_list[-1][1] * (1 - error))) or (data_list[index][1] >= (new_list[-1][1] * (1 + error))):
+            new_list.append(data_list[index])
 
-	print('File reduced by {:.2f}%.'.format(len(data_list) / len(new_list) * 100))
-	print()
+    print('File reduced by {:.2f}%.'.format(len(data_list) / len(new_list) * 100))
+    print()
 
-	return pd.DataFrame(new_list, columns = ['Time (s)', 'Pressure Turbo (mbar)', 'Pressure Evap (mbar)', 'Frequency (Hz)'])
+    return pd.DataFrame(
+        new_list,
+        columns = ['Time (s)', 'Pressure Turbo (mbar)', 'Pressure Evap (mbar)', 'Frequency (Hz)']
+        )
 
 
 def calculate_on_df(dataframe, baseline):
-	dataframe['Time (s)'] = dataframe['Time (s)'] / 60
-	dataframe.rename(columns={'Time (s)':'Time (min)'}, inplace=True)
-	
-	dataframe['Frequency Change (Hz)'] = dataframe['Frequency (Hz)'] - baseline
+    '''Calculates the frequency change from a baseline frequency.'''
+    dataframe['Time (s)'] = dataframe['Time (s)'] / 60
+    dataframe.rename(columns={'Time (s)':'Time (min)'}, inplace=True)
+    dataframe['Frequency Change (Hz)'] = dataframe['Frequency (Hz)'] - baseline
 
-	return dataframe
+    return dataframe
 
 def main(argv):
 
-	inputfile = ""
-	outputfile = ""
-	error = ""
-	baseline = float(5977920)
-	try:
-		opts, args = getopt.getopt(argv,"hi:o:e:b:",["ifile=","ofile=", "error=", "baseline="])
-	except getopt.GetoptError:
-		print('reduce_size.py -i <inputfile> -o <outputfile> -e <error> -b <baseline>')
-		sys.exit()
+    inputfile = ""
+    outputfile = ""
+    error = ""
+    baseline = float(5977920)
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:e:b:",["ifile=","ofile=", "error=", "baseline="])
+    except getopt.GetoptError:
+        print('reduce_size.py -i <inputfile> -o <outputfile> -e <error> -b <baseline>')
+        sys.exit()
 
-	for opt, arg in opts:
-		if opt == '-h':
-			print ('test.py -i <inputfile> -o <outputfile> -e <error> -b <baseline>')
-			sys.exit()
-		elif opt in ("-i", "--ifile"):
-			inputfile = arg
-		elif opt in ("-o", "--ofile"):
-			outputfile = arg
-		elif opt in ("-e", "--error"):
-			error = float(arg)
-		elif opt in ("-b", "--baseline"):
-			if arg == "":
-				pass
-			else:
-				baseline = float(arg)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('test.py -i <inputfile> -o <outputfile> -e <error> -b <baseline>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+        elif opt in ("-e", "--error"):
+            error = float(arg)
+        elif opt in ("-b", "--baseline"):
+            if arg == "":
+                pass
+            else:
+                baseline = float(arg)
 
-		if outputfile == "":
-			outputfile = os.path.basename(inputfile)
-
-
-
-	print('Reducing file {}.'.format(outputfile))
-	df = pd.read_csv(inputfile, sep = '\t')
-	index_pressure = df['Pressure Turbo (mbar)'].where(df['Pressure Turbo (mbar)'] == 1000).last_valid_index()
-
-	df = df.iloc[index_pressure:]
-	df['Time (s)'] = df['Time (s)'].subtract(df['Time (s)'].values[0])
-
-	new_df = calculate_on_df(adaptive_reduce_size(df, error = error), baseline)
+        if outputfile == "":
+            outputfile = os.path.basename(inputfile)
 
 
-	if not os.path.exists('reduced_files\\' + str(error)):
-		os.mkdir('reduced_files\\' + str(error))
 
-	new_df.to_csv('reduced_files\\' + str(error) + '\\' + outputfile, sep = '\t', index = False )
+    print('Reducing file {}.'.format(outputfile))
+    df = pd.read_csv(inputfile, sep = '\t')
+    index_pressure = df['Pressure Turbo (mbar)'].where(df['Pressure Turbo (mbar)'] == 1000).last_valid_index()
+
+    df = df.iloc[index_pressure:]
+    df['Time (s)'] = df['Time (s)'].subtract(df['Time (s)'].values[0])
+
+    new_df = calculate_on_df(adaptive_reduce_size(df, error = error), baseline)
+
+
+    if not os.path.exists('reduced_files\\' + str(error)):
+        os.mkdir('reduced_files\\' + str(error))
+
+    new_df.to_csv('reduced_files\\' + str(error) + '\\' + outputfile, sep = '\t', index = False )
 
 
 if __name__ == '__main__':
-	main(sys.argv[1:])
+    main(sys.argv[1:])
 
 # for file in os.listdir():
-# 	error = 0.01
-# 	if file.endswith(".txt"):
-		# print('Reducing file {}.'.format(file))
-		# df = pd.read_csv(file, sep = '\t')
-		# index_pressure = df['Pressure (mbar)'].where(df['Pressure (mbar)'] == 1000).last_valid_index()
+#   error = 0.01
+#   if file.endswith(".txt"):
+        # print('Reducing file {}.'.format(file))
+        # df = pd.read_csv(file, sep = '\t')
+        # index_pressure = df['Pressure (mbar)'].where(df['Pressure (mbar)'] == 1000).last_valid_index()
 
-		# df = df.iloc[index_pressure:]
-		# df['Time (s)'] = df['Time (s)'].subtract(df['Time (s)'].values[0])
+        # df = df.iloc[index_pressure:]
+        # df['Time (s)'] = df['Time (s)'].subtract(df['Time (s)'].values[0])
 
-		# new_df = calculate_on_df(adaptive_reduce_size(df, error = error))
+        # new_df = calculate_on_df(adaptive_reduce_size(df, error = error))
 
 
-		# if not os.path.exists('reduced_files\\' + str(error)):
-		# 	os.mkdir('reduced_files\\' + str(error))
+        # if not os.path.exists('reduced_files\\' + str(error)):
+        #   os.mkdir('reduced_files\\' + str(error))
 
-		# new_df.to_csv('reduced_files\\' + str(error) + '\\' + file, sep = '\t', index = False )
+        # new_df.to_csv('reduced_files\\' + str(error) + '\\' + file, sep = '\t', index = False )
 
 
 
